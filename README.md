dee73ace42553e89c996631616397c6f3461bbd3
pyrsmk <dev@dreamysource.fr>
2016-01-09 14:12:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+panorama 0.1.0
+==============
+
+Theres a lot websites today showing big background images. But loading images of that size can havea big impact on performance. Panorama tries to solve this problem by using Canvas2D to lighten images footprint.
+
+Install
+-------
+
+```
+npm install pyrsmk-panorama
+bower install pyrsmk-panorama
+```
+
+Use
+---
+
+Quickly :
+
+```js
+// Load the image as the background of .some_block
+panorama($('.some_block')[0], 'images/background.jpg');
+```
+
+But panorama handles some options too as its third parameter :
+
+- sizing : either 'contain' or 'cover' (by default)
+- left : a number or 'center' (by default)
+- top : a number or 'center' (by default)
+
+```js
+// The whole image will be contained in its container
+panorama($('div')[0], 'images/picture.png', {sizing: 'contain'});
+```
+
+Since panorama creates a `canvas` node in absolute position, you should position your container too (at least with `position: relative`);
+
+Advanced use
+------------
+
+Sometimes we could need to manipulate the loaded image used for the canvas. Panorama returns a promise which returns the loaded image. Its useful, per example, to redraw images when the viewport has been resized. Here, we're using [W](https://github.com/pyrsmk/W) to know the viewport size.
+
+```js
+panorama($('div')[0], 'images/picture.png').then(function(image) {
+	W.addListener(function() {
+		// Panorama adds a 'redraw' function into the loaded image
+		image.redraw();
+	});
+});
+```
+
+License
+-------
+
+Published under the [MIT license](http://dreamysource.mit-license.org).
---
+++ src/panorama.js
@@ -0,0 +1,80 @@
+/*! panorama 0.1.0 (https://github.com/pyrsmk/panorama) */
+
+module.exports = function(node, url, options) {
+	
+	// Init canvas
+	var canvas = document.createElement('canvas'),
+		context;
+	if(!(canvas.getContext && (context = canvas.getContext('2d')))) {
+		throw new Error("Canvas2D is not supported by your browser");
+	}
+	canvas.style.position = 'absolute';
+	canvas.style.zIndex = -1;
+	canvas.style.top = 0;
+	canvas.style.left = 0;
+	node.appendChild(canvas);
+	
+	// Load the image
+	var promise = require('pyrsmk-imagine')(url).then(function(images) {
+		// Prepare
+		var image = images[0],
+			x, y,
+			width,
+			height;
+		options.sizing = 'sizing' in options ? options.sizing : 'cover';
+		options.left = 'left' in options ? options.left : 'center';
+		options.top = 'top' in options ? options.top : 'center';
+		// Define the drawing funtion
+		image.redraw = function() {
+			// Resize canvas
+			canvas.width = node.offsetWidth;
+			canvas.height = node.offsetHeight;
+			// Compute the image size
+			switch(options.sizing) {
+				case 'cover':
+					if((image.width / canvas.width) > (image.height / canvas.height)) {
+						width = Math.round((canvas.height / image.height) * image.width);
+						height = canvas.height;
+					}
+					else {
+						height = Math.round((canvas.width / image.width) * image.height);
+						width = canvas.width;
+					}
+					break;
+				case 'contain':
+					if((image.width / canvas.width) > (image.height / canvas.height)) {
+						height = Math.round((canvas.width / image.width) * image.height);
+						width = canvas.width;
+					}
+					else {
+						width = Math.round((canvas.height / image.height) * image.width);
+						height = canvas.height;
+					}
+					break;
+				default:
+					throw new Error("'"+options.sizing+"' sizing option is not supported");
+			}
+			// Compute the image position
+			if(options.left === 'center') {
+				x = (canvas.width - width) / 2;
+			}
+			else {
+				x = options.left;
+			}
+			if(options.top === 'center') {
+				y = (canvas.height - height) / 2;
+			}
+			else {
+				y = options.top;
+			}
+			// Draw image
+			context.drawImage(image, x, y, width, height);
+		};
+		image.redraw();
+		// Return the loaded image instance
+		return image;
+	});
+	
+	return promise;
+	
+};
